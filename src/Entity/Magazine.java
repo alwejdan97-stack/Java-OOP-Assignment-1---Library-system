e94e4681bb4cecabb44dbc6135c5e62181679a79
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 13:58:59 +0400

    set special attributes and add set and get method inside Magazine class

--- src/Entity/Magazine.java
+++ src/Entity/Magazine.java
@@ -3,4 +3,17 @@ package Entity;
 public class Magazine {
     private String editor;
     private String issuNumber;
+
+    public void setEditor(String editor){
+        this.editor=editor;
+    }
+    public String getEditor(){
+        return editor;
+    }
+    public void setIssuNumber(String issuNumber){
+        this.issuNumber=issuNumber;
+    }
+    public String getIsbn(){
+        return issuNumber;
+    }
 }
